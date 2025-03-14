b025f61366006ff42cb4333b01f6251b9bf72ccf
huangzing <89714773+huangzing@users.noreply.github.com>
2025-03-14 15:57:58 +0800

    Add files via upload

---
+++ datasets/dataset_cifar10.py
@@ -0,0 +1,29 @@
+# This file is used to load the CIFAR-10 dataset.
+# 这个文件用于加载CIFAR-10数据集。
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+def get_cifar10_data(batch_size):
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse','ship', 'truck')
+
+    return trainloader, testloader, classes
+
+
